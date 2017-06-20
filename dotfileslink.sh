5d37615283ae61d629cb9d4580cfbb69d4ba5577
shivashin <s14t241@stmail.eng.kagawa-u.ac.jp>
2017-06-21 00:58:42 +0900

    zplug modify

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -2,5 +2,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zplug ~/.zplug
+ln -sf ~/dotfiles/zplug ~/zplug
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
