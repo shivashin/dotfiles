cf99e73a16ab6526a193d1d69380f3f0ff6fa3d6
shivashin <s14t241@stmail.eng.kagawa-u.ac.jp>
2017-06-21 13:42:52 +0900

    modify links

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
+#ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/zplug ~/zplug
+#ln -sf ~/dotfiles/zplug ~/zplug
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
