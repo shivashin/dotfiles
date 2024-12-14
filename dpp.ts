add376eb7ae819337f48619124eda1472ad18151
ryo.shibahara <ryo.shibahara@aktsk.jp>
2024-12-14 11:36:06 +0900

    Fix file path to correct name

--- dpp.ts
+++ dpp.ts
@@ -32,7 +32,7 @@ export class Config extends BaseConfig {
     });
 
     const [context, options] = await args.contextBuilder.get(args.denops);
-    const dotfilesDir = "~/dotfiles/toml/";
+    const dotfilesDir = "~/dotfiles/tomls/";
 
     // Load toml plugins
     const tomls: Toml[] = [];
